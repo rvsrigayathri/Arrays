bcf6e8864cbcd43b151df0fc2a0a3ef5b72d508b
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-09 06:11:11 +0530

    Add files via upload

---
+++ smallest_element_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int arr[100],i,n,min;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	min=arr[0];
+	for(i=0;i<n;i++){
+		if(arr[i]<min){
+			min=arr[i];
+		}
+	}
+	printf("%d",min);
+	return 0;
+}
