648577cfaa4de086250fdf832ff65dda850c1a05
R.Srigayathri <r.v.srigayathri@gmail.com>
2026-07-21 06:00:34 +0530

    Add files via upload

---
+++ replace_even_numbers_with_minusone.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int arr[100],i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++){
+		if(arr[i]%2==0){
+			arr[i]=-1;
+		}
+	}
+	printf("Modified array: \n");
+	for(i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+	return 0;
+}
